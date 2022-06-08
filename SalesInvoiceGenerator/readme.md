c52e5972f0c4fc77380d32700fe3bcd19553ee08
Ola Galal <olaglal97@hotmail.com>
2022-06-08 15:19:19 +0200

    classes and objects

---
+++ SalesInvoiceGenerator/readme.md
@@ -0,0 +1,7 @@
+# Sales Invoice Generator
+
+## Sceenshots
+![Alt text]("")
+
+## License
+This project is available under the terms of [MIT License](https://choosealicense.com/licenses/mit/)
---
+++ SalesInvoiceGenerator/src/main/java/model/InvoiceLine.java
@@ -0,0 +1,65 @@
+package model;
+
+/**
+ *
+ * @author Ola Galal
+ */
+public class InvoiceLine {
+
+    String itemName;
+    float itemPrice;
+    int itemCount;
+    float itemTotal;
+    InvoiceHeader mainInvoice;
+    
+    public InvoiceLine() {
+    }
+
+    public InvoiceLine(String itemName, float itemPrice, int itemCount, InvoiceHeader mainInvoice) {
+        this.itemName = itemName;
+        this.itemPrice = itemPrice;
+        this.itemCount = itemCount;
+        this.mainInvoice = mainInvoice;
+    }
+
+    public String getItemName() {
+        return itemName;
+    }
+
+    public void setItemName(String itemName) {
+        this.itemName = itemName;
+    }
+
+    public float getItemPrice() {
+        return itemPrice;
+    }
+
+    public void setItemPrice(float itemPrice) {
+        this.itemPrice = itemPrice;
+    }
+
+    public int getItemCount() {
+        return itemCount;
+    }
+
+    public void setItemCount(int itemCount) {
+        this.itemCount = itemCount;
+    }
+
+    public float getItemTotal() {
+        return itemTotal;
+    }
+
+    public void setItemTotal(float itemTotal) {
+        this.itemTotal = itemTotal;
+    }
+
+    public InvoiceHeader getMainInvoice() {
+        return mainInvoice;
+    }
+
+    public void setMainInvoice(InvoiceHeader mainInvoice) {
+        this.mainInvoice = mainInvoice;
+    }
+
+}
